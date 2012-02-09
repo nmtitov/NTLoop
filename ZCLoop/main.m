615aa7578e064e9124a8eb00432f35f3c274f0cf
Nikita Titov <nmtitov@yandex.ru>
2012-02-09 18:19:56 +0400

    Initial Commit

---
+++ ZCLoop/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZCLoop
+//
+//  Created by Nikita Titov on 2/9/12.
+//  Copyright (c) 2012 nikita@zencode.ru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZCAppDelegate class]));
+    }
+}
