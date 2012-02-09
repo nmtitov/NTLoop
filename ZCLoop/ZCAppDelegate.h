7f10a4f7c51a4fb05f3a3a3db00d8129e2c115fc
Nikita Titov <nikita@zencode.ru>
2012-02-09 18:19:56 +0400

    Initial Commit

---
+++ ZCLoop/ZCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZCAppDelegate.h
+//  ZCLoop
+//
+//  Created by Nikita Titov on 2/9/12.
+//  Copyright (c) 2012 nikita@zencode.ru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
