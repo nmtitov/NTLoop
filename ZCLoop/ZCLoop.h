900c2e246c4b289ff513e3a51c39b1446de4c153
Nikita Titov <nmtitov@yandex.ru>
2012-02-28 12:28:35 +0400

    removed legacy naming prefix

--- ZCLoop/ZCLoop.h
+++ ZCLoop/ZCLoop.h
@@ -9,23 +9,23 @@
 
 @class ZCLoop;
 
-@protocol TRLoopDelegate <NSObject>
+@protocol ZCLoopDelegate <NSObject>
 @required
-- (void)trLoopUpdate:(ZCLoop *)loop;
+- (void)zcLoopUpdate:(ZCLoop *)loop;
 @optional
-- (void)trLoop:(ZCLoop *)loop willPause:(BOOL)pause;
+- (void)zcLoop:(ZCLoop *)loop willPause:(BOOL)pause;
 @end
 
 typedef void (^UpdateBlock)(NSTimeInterval timeSinceLastUpdate);
 
-// The TRLoop class provides part of the standard view controller functionality, it implements loop using CADisplayLink (TODO: add NSTimer for compatibility with old devices). 
-// A TRLoop object usually works in conjunction with a view object to display frames of animation in the view or to update application logic using time interval since last update.
+// The ZCLoop class provides part of the standard view controller functionality, it implements loop using CADisplayLink (TODO: add NSTimer for compatibility with old devices). 
+// A ZCLoop object usually works in conjunction with a view object to display frames of animation in the view or to update application logic using time interval since last update.
 
-// To use this class, allocate and initialize a new TRLoop subclass with frame intrerval (1 means 60 frames per second, 2 means 30 frames per secon and so on) using update block object or set its delegate. 
+// To use this class, allocate and initialize a new ZCLoop subclass with frame intrerval (1 means 60 frames per second, 2 means 30 frames per secon and so on) using update block object or set its delegate. 
 // You can set a delegate or configure other properties on the view controller, such as whether the loop is automatically paused or resumed when the application moves into the background.
 
 // When active, loop automatically updates application data using time interval since last update.
-// The view controller calls its delegate’s trLoopUpdate: method. If you use delegate then your delegate should update data that does not involve rendering the results to the screen.
+// The view controller calls its delegate’s zcLoopUpdate: method. If you use delegate then your delegate should update data that does not involve rendering the results to the screen.
 
 @interface ZCLoop : NSObject
 
@@ -33,11 +33,11 @@ typedef void (^UpdateBlock)(NSTimeInterval timeSinceLastUpdate);
 + (id)loopWithFrameInterval:(NSUInteger)frameInterval usingBlock:(UpdateBlock)block;
 // Designated initializer. Block variable |block| will be copied and assigned to private variable
 - (id)initWithFrameInterval:(NSUInteger)frameInterval usingBlock:(UpdateBlock)block;
-// Initialize loop without block. User should use delegate adopting |TRLoopDelegate| protocol
+// Initialize loop without block. User should use delegate adopting |ZCLoopDelegate| protocol
 - (id)initWithFrameInterval:(NSUInteger)frameInterval;
 
 // The view controller’s delegate.
-@property (unsafe_unretained, nonatomic) id<TRLoopDelegate> delegate;
+@property (unsafe_unretained, nonatomic) id<ZCLoopDelegate> delegate;
 // The number of frame updates that have been sent by the view controller since it was created. (read-only)
 @property(nonatomic, readonly) NSInteger framesDisplayed;
 // The actual rate that the view controller attempts to call the view to update its contents. (read-only)
